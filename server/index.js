a50001e5652185819f6e0380158c32b4c0d6804f
firas-debich <debichf02@gmail.com>
2022-05-28 16:25:05 +0100

    youtube downloder works

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const asyncHandler = require("express-async-handler");
+const path = require("path");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "/youtube-downloader/dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname1, "youtube-downloader", "dist", "index.html")
+  );
+});
+app.post("/download", (req, res) => {
+  const url = req.body.url;
+  
+  ytdl
+    .getBasicInfo(url)
+    .then((info) => {
+      const title = info.videoDetails.title;
+      console.log(title);
+      res.header("Content-Disposition", `attachment: filename=${title}.mp4`);
+      ytdl(url, {
+        quality: "highest",
+        format: "mp4",
+      }).pipe(res);
+    })
+    .catch((err) => console.log(err));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- youtube-downloader/src/App.jsx
+++ youtube-downloader/src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useState } from "react";
+import axios from "axios";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Donwload video or music from youtube</h1>
+      <div>
+        <form action="/download" method="post">
+          <input
+            name="url"
+            placeholder="Video url e.g https://youtube.com/watch?v=azfze"
+          />
+          <button type="submit">Download now!!</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
