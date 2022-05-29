0aa70325af6e7c35a4a9869f6927d3f51b77f357
firas-debich <debichf02@gmail.com>
2022-05-29 18:58:29 +0100

    deploy to heroku

--- server/index.js
+++ server/index.js
@@ -1,30 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
-const asyncHandler = require("express-async-handler");
 const path = require("path");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "/youtube-downloader/dist")));
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendFile(
     path.resolve(__dirname1, "youtube-downloader", "dist", "index.html")
   );
 });
 app.post("/download", (req, res) => {
   const url = req.body.url;
-  
   ytdl
     .getBasicInfo(url)
     .then((info) => {
       const title = info.videoDetails.title;
-      console.log(title);
       res.header("Content-Disposition", `attachment: filename=${title}.mp4`);
       ytdl(url, {
         quality: "highest",
--- youtube-downloader/src/App.jsx
+++ youtube-downloader/src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
-import axios from "axios";
+
+
 const App = () => {
   return (
     <div>
-      <h1>Donwload video or music from youtube</h1>
-      <div>
-        <form action="/download" method="post">
+      <h1 className="text-center mt-5 text-3xl font-bold underline mb-5 text-red-500">
+        Donwload mp4
+      </h1>
+      <div className="flex justify-center items-center w-[100%]">
+        <form
+          action="/download"
+          method="post"
+          className="flex justify-center sm:flex-col items-center  w-[80%] "
+        >
           <input
+            className="bg-gray-200 w-full flex-1  sm:flex-none sm:mb-5   appearance-none mx-5 h-16 rounded px-5"
             name="url"
             placeholder="Video url e.g https://youtube.com/watch?v=azfze"
           />
-          <button type="submit">Download now!!</button>
+          <button type="submit" className="bg-indigo-200  sm:w-full rounded p-5">
+            Download now!!
+          </button>
         </form>
       </div>
     </div>
